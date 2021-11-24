02376ece4fcd05d6c51ff4b0710b35b42c376f56
Louis Cavecchi <cavecchilouis@gmail.com>
2021-11-24 20:42:50 +0100

    Hero section

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,30 @@
-import Head from 'next/head'
-import Images from 'next/image'
-import styles from '../styles/components/Header/Home.module.scss'
-import { useEffect, useRef } from 'react'
-import logo from '../public/assets/img/logo.png'
+import Head from "next/head";
+import Images from "next/image";
+import styles from "../styles/components/Header/Header.module.scss";
+import { useEffect, useRef } from "react";
+import logo from "../public/assets/img/logo.png";
 
 export default function Header() {
-    const navi = useRef(null)
-    useEffect(() => {
-        let sections = document.querySelectorAll('section'),
-            compteur = document.querySelector('h1'),
-            nav = document.querySelector('nav')
-            console.log(navi.current)
-    })
+  useEffect(() => {
+    let sections = document.querySelectorAll("section"),
+      compteur = document.querySelector("h1"),
+      nav = document.querySelector("nav");
+
+    sections.forEach((el, i) => {
+     nav.innerHTML += `<a href="#${el.id}"><div></div></a>`
+    });
+
+    document.querySelectorAll("nav a").forEach((el, i) => {
+      el.addEventListener("click", (e) => {
+        document.querySelectorAll("nav a").forEach((el, i) => {
+        });
+      });
+    });
+  });
   return (
     <header className={styles.header}>
-        <Images src={logo}></Images>
-        <nav className={styles.navigation} ref={navi}></nav>
+      <Images src={logo} className={styles.logo} height="50" width="50"></Images>
+      <nav className={styles.navigation}></nav>
     </header>
-  )
+  );
 }
---
+++ components/Hero.js
@@ -0,0 +1,41 @@
+import styles from "../styles/components/Hero/Hero.module.scss";
+import { useEffect, useRef, useState } from "react";
+import Header from '../components/Header'
+
+export default function Hero() {
+  const heroBackground = useRef()
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+        if(window.scrollY < 1000) heroBackground.current.style.transform = `scale(${(window.scrollY + 250) * 2})`;
+    })  
+  });
+
+  return (
+    <section id={"1"} className={styles.hero}>
+        <Header />
+        <div>
+            <h1 className={styles.hero__text}><span>Lorem ipsum dolor sit amet,</span><span> consectetur adipiscing elit.</span></h1>
+            <div className={styles.hero__background} ref={heroBackground}></div>
+        </div>
+        <div className={styles.hero__footer}>
+            <div></div>
+            <p>#entregens2021</p>
+            <div className={styles.hero__social}>
+                <div className={styles.hero__social__block}>
+                    <div className={styles.hero__social__wrapper}></div>
+                </div>
+                <div className={styles.hero__social__block}>
+                    <div className={styles.hero__social__wrapper}></div>
+                </div>
+                <div className={styles.hero__social__block}>
+                    <div className={styles.hero__social__wrapper}></div>
+                </div>
+            </div>
+        </div>
+        <section id={"2"} className={styles.hero}></section>
+        <section id={"3"} className={styles.hero}></section>
+
+    </section>
+  );
+}
