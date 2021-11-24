33ec7024439403b408d79a737fb395e985ac5300
Louis Cavecchi <cavecchilouis@gmail.com>
2021-11-25 00:07:41 +0100

    Add 3D and fonts

--- components/Reassurances.js
+++ components/Reassurances.js
@@ -1,35 +1,39 @@
 import styles from '../styles/components/Reassurances/Reassurance.module.scss'
+import Image from 'next/image'
 
 export default function Reassurances() {
     let data = [
         {
             title: 'Ludo',
-            text: 'Lortestetettetete',
-            image: 'https://images.pexels.com/photos/6663884/pexels-photo-6663884.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+            text: 'Integer blandit elit, velit turpis. Eget magna at pretium sit accumsan, nisi, pellentesque bibendum mollis. Libero id quis at sed facilisis.',
+            image: 'https://images.pexels.com/photos/4115538/pexels-photo-4115538.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         },
         {
             title: 'Tibo',
-            text: 'Lortestetettetete',
-            image: 'https://images.pexels.com/photos/6663884/pexels-photo-6663884.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+            text: 'Integer blandit elit, velit turpis. Eget magna at pretium sit accumsan, nisi, pellentesque bibendum mollis. Libero id quis at sed facilisis.',
+            image: 'https://images.pexels.com/photos/4115538/pexels-photo-4115538.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         },
         {
             title: 'Loris',
-            text: 'Lortestetettetete',
-            image: 'https://images.pexels.com/photos/6663884/pexels-photo-6663884.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+            text: 'Integer blandit elit, velit turpis. Eget magna at pretium sit accumsan, nisi, pellentesque bibendum mollis. Libero id quis at sed facilisis.',
+            image: 'https://images.pexels.com/photos/4115538/pexels-photo-4115538.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         },
         {
             title: 'Paulo',
-            text: 'Lortestetettetete',
-            image: 'https://images.pexels.com/photos/6663884/pexels-photo-6663884.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+            text: 'Integer blandit elit, velit turpis. Eget magna at pretium sit accumsan, nisi, pellentesque bibendum mollis. Libero id quis at sed facilisis.',
+            image: 'https://images.pexels.com/photos/4115538/pexels-photo-4115538.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         }
     ]
   return (
-    <section id={"2"}>
-        {
-            data.map((elt, i) => {
-                return <ReassurancesBlock key={i} title={elt.title} text={elt.text} image={elt.image} />
-            })
-        }
+    <section id={"2"} className={styles.reassurances}>
+        <div className={styles.reassurances__container}>
+            <h2>Turpis a egestas <span>senectus habit,</span></h2>
+            {
+                data.map((elt, i) => {
+                    return <ReassurancesBlock key={i} title={elt.title} text={elt.text} image={elt.image} />
+                })
+            }
+        </div>
     </section>
   )
 }
@@ -37,12 +41,11 @@ export default function Reassurances() {
 export function ReassurancesBlock(props) {
   return (
     <div className={styles.reassurance}>
-        <img className={styles.reassuranceImage} src={props.image}></img>
+        <Image className={styles.reassuranceImage} src={`${props.image}`} width={60} height={60}/>
         <div className={styles.reassuranceDetails}>
-            <p className={styles.reassuranceTitle}>{props.title}</p>
+            <h3 className={styles.reassuranceTitle}>{props.title}</h3>
             <p className={styles.reassuranceText}>{props.text}</p>
         </div>
-        
     </div>
   )
 }
