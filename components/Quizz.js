a5150179a4976355530c6db693c46099231bbe71
Hubert <erwan.marques@yahoo.fr>
2021-12-03 00:09:55 +0100

    quizz

--- components/Quizz.js
+++ components/Quizz.js
@@ -2,117 +2,118 @@ import styles from '../styles/components/Quizz.module.scss'
 import { useEffect, useRef } from 'react'
 
 export default function Quizz() {
-    let quizzdata = [
-        {
-            question: 'zaejazlkeza ?',
-            reponse : [
-                {
-                    title: 'reponse1',
-                    value: 50,
-                    correct: true
-                },
-
-                {
-                    title: 'reponse2',
-                    value: 0,
-                    correct: false
-                },
-
-                {
-                    title: 'reponse3',
-                    value: 0,
-                    correct: false
-                }
-            ]
-        },
-        {
-            question: 'zaejazlkeza ?',
-            reponse : [
-                {
-                    title: 'reponse1',
-                    value: 50,
-                    correct: true
-                },
-
-                {
-                    title: 'reponse2',
-                    value: 0,
-                    correct: false
-                },
-
-                {
-                    title: 'reponse3',
-                    value: 0,
-                    correct: false
-                }
-            ]
-        },
-        {
-            question: 'zaejazlkeza ?',
-            reponse : [
-                {
-                    title: 'reponse1',
-                    value: 50,
-                    correct: true
-                },
-
-                {
-                    title: 'reponse2',
-                    value: 0,
-                    correct: false
-                },
-
-                {
-                    title: 'reponse3',
-                    value: 0,
-                    correct: false
-                }
-            ]
-        }
+    let questions = [{
+        titre: "Avez-vous un projet concret ?",
+        necessary: true,
+        index: 0,
+        answers: [{
+            titre: "Oui",
+            value: 100
+          },
+          {
+            titre: "Non",
+            value: -1
+          }
+        ]
+      },
+      {
+        titre: "Combien d'année d'expérience avez-vous dans ce milieu ?",
+        necessary: true,
+        index: 1,
+        answers: [{
+            titre: "0 - 1",
+            value: 100
+          },
+          {
+            titre: "2 - 5",
+            value: 200
+          },
+          {
+            titre: "5 - 7",
+            value: 300
+          }
+        ]
+      },
+      {
+        titre: "Que pensez-vous du télétravail à titre personnel ?",
+        necessary: true,
+        index: 2,
+        answers: [{
+            titre: "Oui à full time je kiff",
+            value: 300
+          },
+          {
+            titre: "Oui à temps partiel",
+            value: 200
+          },
+          {
+            titre: "None",
+            value: 0
+          }
+        ]
+      }
     ]
-
+    
     const quiz = useRef()
     useEffect(() => {
-        let options = {
-            threshold: 0.8
-        };
-        const io = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting ) {
-                document.querySelectorAll('[data-scroll="nav"]').forEach(elt => {
-                    elt.style.backgroundColor = "white"
-                });
-                document.querySelector('[data-scroll="bg"]').style.backgroundColor = "black"
-            } else {
-                document.querySelector('[data-scroll="bg"]').style.backgroundColor = "#F8F8F8"
-                document.querySelectorAll('[data-scroll="nav"]').forEach(elt => {
-                    elt.style.backgroundColor = "black"
-                });
-            }
+        let container = document.querySelector('form')
+        
+        let currentQuestion = []
+        let indexCurrent = 0;
+        let score = 0;
+        
+        
+        const scoring = (currentScore) => {
+        score += currentScore
+        document.getElementById('score').innerHTML = score
+        }
+        
+        document.querySelector('[data-button-next]').addEventListener('click', function(){
+            document.querySelectorAll('input').forEach((elt) => {
+                if (elt.checked) scoring(parseInt(elt.value))
+            })
+            indexCurrent++;
+            changeQuestion(indexCurrent);
         })
-        }, options)
-        //o.observe(quiz.current)
-    })
 
+        
+        const changeQuestion = (id) => {
+        
+        currentQuestion.push(questions.filter(question => question.index == id))
+        currentQuestion.map((elt, i) => {
+            if(i >= questions.length) {
+                    if(score > 600) alert("Mashalah t'as gagné")
+            else  {
+                score = 0
+                document.querySelector('h2').innerHTML = score
+                alert('Tu pues') 
+                }
+            }
+            let question = elt[0]
+            document.getElementById('totalQuestions').innerHTML = `${i + 1} - ${questions.length}`
+            container.innerHTML = `<h2>${question.titre}</h2>
+                ${question.answers.map((elt,i) => {
+            return `<div>
+                        <input type='radio' name='${elt.id}' value='${elt.value}' required>
+                                <label for='${elt.id}'>${elt.titre}</label>
+                    </div>`
+            }).join('')}`
+        })
+        }
+        
+        changeQuestion(0)
+    })
   return (
     <section id={"4"} className={styles.quiz} ref={quiz}>
         {
-            quizzdata.map((elt, i) => (
-                <div key={i}>
-                    <h3>{elt.question}</h3>
-                    <div>
-                        {
-                            elt.reponse.map((eltreponse, index) => (
-                                <div key={index}>
-                                    <label>{eltreponse.title}</label>
-                                    <label>{eltreponse.value}</label>
-                                    <label>{eltreponse.correct}</label>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
-            ))
+            <div>
+                <h1>Quizz</h1>
+                <h2 id={'score'}>0</h2><p id={'totalQuestions'}></p>
+                <form></form>
+                <button className={styles.next} data-button-next>
+                    Question Suivante
+                </button>
+            </div>
         }
     </section>
   )
