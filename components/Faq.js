54a53727850cef2a0c970802b7662bdfd1c253d4
Hubert <erwan.marques@yahoo.fr>
2021-12-05 00:20:27 +0100

    Faq modifications

--- components/Faq.js
+++ components/Faq.js
@@ -4,19 +4,19 @@ import { useState } from 'react'
 export default function Faq() {
     let faqdata = [
         {
-            title: 'Question 1',
+            title: 'Question numéro 1',
             text: 'Quelle est la meilleure et la pire chose dans le fait d\'être un homme? Quel film aimez-vous tant que vous pouvez regarder à plusieurs reprises ?'
         },
         {
-            title: 'Question 2',
+            title: 'Question numéro 2',
             text: 'Que feriez-vous / ne feriez-vous pas pour un million de dollars? Quelle est une chose que vous appréciez quand vous étiez enfant et que vous appréciez encore ?'
         },
         {
-            title: 'Question 3',
+            title: 'Question numéro 3',
             text: 'Quelle est la situation de pression la plus élevée dans laquelle vous ayez jamais été et comment l avez-vous gérée? Qu est-ce qui vous stresse ?'
         },
-        {
-            title: 'Question 4',
+        { 
+            title: 'Question numéro 4',
             text: 'Quelle personne ou quel événement a eu le plus grand impact sur votre vie ? Si vous pouviez faire une chose chaque jour de votre vie pour le reste de votre vie, quelle serait-elle ?'
         }
     ]
@@ -33,7 +33,7 @@ export default function Faq() {
                 <div key={i} className={selected === i ? styles.faqBlock+' '+ styles.show : styles.faqBlock} onClick={() => toggle(i)}>
                     <div className={styles.faqHeader}>
                         <h2>{elt.title}</h2>
-                        <div className={selected === i ? styles.faqCrossContainer+' '+styles.isOpen : styles.faqCrossContainer}>
+                        <div id={"crossbar"+i} className={selected === i ? styles.faqCrossContainer+' '+styles.isOpen : styles.faqCrossContainer}>
                             <span></span>
                             <span></span>
                         </div>
