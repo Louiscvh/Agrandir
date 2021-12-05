c4bc95c9cc147d27103a8f9599dc5b8fe8942ad1
Hubert <erwan.marques@yahoo.fr>
2021-12-05 23:59:35 +0100

    Block témoignage

--- components/Infos.js
+++ components/Infos.js
@@ -38,13 +38,14 @@ export default function Infos() {
     return (
         <section id={"2"} className={styles.infos}>
             <div className={styles.infosContainer} ref={infos}>
-                <h2><span data-up>Entregens t’accompagne</span> dans tes démarches ✨</h2>
-                <p className={styles.infosTitleSpan}>Tristique diam sollicitudin praesent</p>
-                    {
-                        data.map((elt, i) => {
-                            return <InfosBlock key={i} title={elt.title} text={elt.text} />
-                        })
-                    }
+                <h2><span data-up>Entregens t’accompagne</span><br></br> dans tes démarches ✨</h2>
+                <div className={styles.infosContent} data-appear>
+                    <div className={styles.infoDetails}>
+                        <h3 className={styles.infoTitle}>Un réseau de plus de <span>100 000 entrepreneurs !</span></h3>
+                        <p className={styles.infoText}>Sur 900 000 entrepreneurs en France, on a réunis une communauté de 100 000 entrepreneurs français: Web, Artisanat, Art, Marketing, Vente... Les entrepreneurs de notre communauté viennent de tous les secteurs possibles et imaginables, ce qui fait la force de notre association.<br></br><br></br>
+                        Sur entre-aide.fr, tu pourras retrouver un catalogue complet d’articles, conseils et informations vérifiés et écrient par des professionnels qui ont éprouvés leurs compétences dans leur domaine, ces articles traitent de l’entrepreneuriat et de tout ce qui gravite autour, santé mentale, argent, amis, télétravail etc... En plus de ça tu peux rentrer en contact avec ces professionnels et tous les autres membres de la communauté (y compris nous) grâce à nos salons textuels.</p>
+                    </div>
+                </div>
             </div>
         </section>
     )
